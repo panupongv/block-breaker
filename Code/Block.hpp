7b67f74522c7c0d860ff5244cbad0599d7718a87
= <->
2017-03-18 20:04:39 +0700

    modify Block constructor to accept float pixel position

--- Code/Block.hpp
+++ Code/Block.hpp
@@ -5,7 +5,7 @@
 class Block : public Sprite
 {
 public:
-	Block(std::string texture_name, int grid_x, int grid_y);
+	Block(std::string texture_name, float x, float y);
 	void update();
 	void checkCollision(); //Ball& ball
 	void setGridPosition(int x, int y);
